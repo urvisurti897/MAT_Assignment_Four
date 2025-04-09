2deaa08fb010999415e7b7556b5c1888e4a880a4
Urvi <urvisurti897@gmail.com>
2025-04-09 15:10:55 -0400

    calculateCanvasSize.test.ts, calculateCanvasSize.ts: Updated code in both the files and fixed error

---
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -0,0 +1,29 @@
+import calculateCanvasSize from "./calculateCanvasSize";
+
+describe("calculateCanvasSize", () => {
+  test("returns correct result", () => {
+    const result = calculateCanvasSize("10", "100");
+    expect(result).toEqual(1000);
+  });
+
+  test("should throw an error if length is negative", () => {
+    expect(() => calculateCanvasSize("-10", "100")).toThrow("Length should be positive");
+  });
+
+  test("should throw an error if width is negative", () => {
+    expect(() => calculateCanvasSize("10", "-100")).toThrow("Width should be positive");
+  });
+
+
+  test("should throw an error if length is zero", () => {
+    expect(() => calculateCanvasSize("0", "100")).toThrow("Length should not be zero");
+  });
+
+  test("should throw an error if width is zero", () => {
+    expect(() => calculateCanvasSize("10", "0")).toThrow("Width should not be zero");
+  });
+
+  test.todo("should throw an error if length is non-numeric");
+
+  test.todo("should throw an error if width is non-numeric");
+});
--- src/calculateCanvasSize/calculateCanvasSize.ts
+++ src/calculateCanvasSize/calculateCanvasSize.ts
@@ -2,5 +2,32 @@ export default function calculateCanvasSize(
   length: string,
   width: string
 ) {
-  return parseInt(length) * parseInt(width);
+
+  const lengthNum = parseInt(length);
+  const widthNum = parseInt(width);
+
+  if (lengthNum < 0){
+    throw new Error("Length should be positive");
+  }
+
+  if (widthNum < 0){
+    throw new Error("Width should be positive");
+  }
+
+  if (lengthNum == 0){
+    throw new Error("Length should not be zero");
+  }
+
+  if (widthNum == 0){
+    throw new Error("Width should not be zero");
+  }
+
+  if (isNaN(lengthNum)) {
+    throw new Error("Length should not be non-numeric");
+  }
+  if (isNaN(widthNum)) {
+    throw new Error("Width should not be non-numeric");
+  }
+
+  return (lengthNum) * (widthNum);
 }
