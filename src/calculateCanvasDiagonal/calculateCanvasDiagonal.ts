7a6acbac3fba1620045127e695733818d286cf25
Urvi <urvisurti897@gmail.com>
2025-04-09 14:42:11 -0400

    Updated code in calculateCanvasDiagonal.test.ts & calculateCanvasDiagonal.ts files and fixed the error

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
@@ -1,6 +1,9 @@
-export default function calculateCanvasSize(
+export default function calculateCanvasDiagonal(
   length: string,
   width: string
-) {
-  return 2 * (parseInt(length) + parseInt(width));
+): number {
+  const l = Math.abs(parseFloat(length));
+  const w = Math.abs(parseFloat(width));
+  if (isNaN(l) || isNaN(w)) return NaN;
+  return Math.sqrt(l ** 2 + w ** 2);
 }
